87a0cc156b1ec46812402dcb553152a9b87509cd
Maqerfoo <jakobpoulsen93@gmail.com>
2023-10-08 21:55:54 +0200

    init

---
+++ LabelingAlg .jl
@@ -0,0 +1,196 @@
+struct Instance
+    n_tasks::Int64
+    avg_speed::Float64
+    workhours::Float64
+    volume_capacity::Float64
+    weight_capacity::Float64
+    coord_x::Array{Float64,1}
+    coord_y::Array{Float64,1}
+    service_time::Array{Float64,1}
+    parts_weight::Array{Float64,1}
+    parts_volume::Array{Float64,1}
+    distance::Array{Float64,2}
+end
+
+struct Edge
+    from::Int64
+    to::Int64
+    cost::Any
+    res_usage::Array{Any,1}
+end
+
+struct Graph
+    V::Array{Int64,1}
+    A::Array{Array{Edge,1},1}
+end
+
+struct Label
+    vertex::Int64
+    s::Int64 # visited count
+    visited::Array{Bool,1} # visited set
+    cost::Int64 # cost
+    prev# previous label, or the value "missing" if none exist
+end
+
+function instance_reader(filename)
+    file = open(filename)
+
+    vals = split(readline(file))
+    n_tasks = parse(Int64,vals[1])
+    avg_speed = parse(Float64,vals[2])
+    workhours = parse(Float64,vals[3])
+    volume_cap = parse(Float64,vals[4])
+    weight_cap = parse(Float64,vals[5])
+
+    coord_x = zeros(n_tasks)
+    coord_y = zeros(n_tasks)
+    service_time = zeros(n_tasks)
+    parts_weight = zeros(n_tasks)
+    parts_volume = zeros(n_tasks)
+    dist = zeros(n_tasks,n_tasks)
+
+    for t in 1:n_tasks
+        vals = split(readline(file))
+        coord_x[t] = parse(Float64,vals[1])
+        coord_y[t] = parse(Float64,vals[2])
+        parts_volume[t] = parse(Float64,vals[3])
+        parts_weight[t] = parse(Float64,vals[4])
+        service_time[t] = parse(Float64,vals[5])
+    end
+
+    for t in 1:n_tasks
+        vals = split(readline(file))
+        dist[t,:] = parse.(Float64,vals)
+    end
+
+    close(file)
+
+    return Instance(n_tasks,avg_speed,workhours,
+                    volume_cap, weight_cap,
+                    coord_x, coord_y,
+                    service_time,parts_weight,
+                    parts_volume,dist)
+end
+
+function graph_constructor(inst)
+    n_vertices = inst.n_tasks
+    res_high = [inst.volume_capacity, inst.weight_capacity]
+    #n_arcs = n_vertices^2
+    A = [Edge[] for v in 1:n_vertices]
+    for i in 1:n_vertices, j in 1:n_vertices 
+        if i!= j
+            push!(A[i],Edge(i,j,-1,[inst.parts_volume[j], inst.parts_weight[j]]))
+        end
+    end
+    return Graph(collect(1:n_vertices),A), res_high
+end
+
+function is_equal(a::Label, b::Label)
+    #TODO: Complete this function
+end
+function is_less(a::Label, b::Label)
+    #TODO: Complete this function
+end
+
+function dominates(a::Label, b::Label)
+    if is_equal(a,b) || is_less(a,b)
+        return true
+    end
+    return false
+end
+
+# Label constructor used to generate the first label
+function Label(source,V)
+    v = zeros(Int64,length(V))
+    v[source] = 1
+    return Label(source,1,v,0,missing)
+end
+
+# Extends a label from an edge toward the vertex v
+function extend(e::Edge,λ::Label,v::Int64)
+    if λ.visited[v]
+        return Nothing
+    end
+    visited = deepcopy(λ.visited)
+    visited[v] = 1
+    return Label(v,λ.s+1,visited,λ.cost+e.cost,λ)
+end
+
+# Applies the dominance rule. Can be improved by keeping the labels sorted
+function dominance(Λ::Array{Label,1},l::Label)
+    changed = false
+    A = []
+    if isempty(Λ)
+        changed = true
+        push!(A,l)
+    else
+        for λ in Λ
+            if dominates(λ,l)
+                return Λ, false
+            elseif dominates(l,λ)
+                if !changed
+                    changed = true
+                    push!(A,l)
+                end
+            else
+                push!(A,λ)
+            end
+        end
+    end
+    if !changed
+        changed = true
+        push!(A,l)
+    end
+    return A, changed
+end
+
+function labeling_ERCSP(G,source)
+
+    # INITIALIZE-SINGLE-SOURCE
+    # Generate the set of empty labels for each vertex in the graph
+    Λ = [Label[] for v in G.V]
+    # Add the initialal label for the source
+    push!(Λ[source],Label(source,G.V))
+
+    # MAIN-BODY
+    # The set of vertices to extend
+    Q = Set{Int64}()
+    # Adding the source to the vertices to extend
+    push!(Q,source)
+
+    # while we still have vertices to extend
+    while !isempty(Q)
+        # get the next vertex to extend
+        u = pop!(Q)
+
+        # for each edge going out of the vertex
+        for e in G.adj[u]
+            # get the next vertex
+            v = e.to
+            # flag to check if the labels changed
+            changed = false
+            # for each label of the vertex to extend
+            for λ in Λ[u]
+                # extend the label
+                l=extend(e,λ,v)
+                if l!=Nothing # we could extend
+                    # apply the dominance rule
+                    Λ[v], ch = dominance(Λ[v],l)
+                    # update the changed flag
+                    changed = changed || ch
+                end
+            end
+            # if the labels have been updated
+            if changed
+                # add vertex v to the list of nodes to extend
+                push!(Q,v)
+            end
+        end
+    end
+    return Λ
+end
+
+
+inst = instance_reader("Data/instance1.txt")
+G, res_max = graph_constructor(inst)
+
